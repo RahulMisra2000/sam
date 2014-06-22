d1c597918a17a0a432c3fb5dc1afbada8d0ec1f8
Rahul Misra <RahulMisra2000@gmail.com>
2014-06-22 11:51:58 -0400

    Initial

---
+++ ThisOldSpouseNet4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ThisOldSpouseNet4.MvcApplication" Language="C#" %>
---
+++ ThisOldSpouseNet4/Scripts/webtickercustom.js
@@ -0,0 +1,24 @@
+﻿$(function () {
+    $("#webticker").webTicker({speed: 50});
+    //$("#webticker2").webTicker({ duplicate: true, speed: 40, direction: 'right', rssurl: 'http://yourwebsite.com/rss/', rssfrequency: 1, startEmpty: false, hoverpause: false });
+
+    //$("#stop").click(function () {
+    //    $("#webticker").webTicker('stop');
+    //});
+
+    //$("#continue").click(function () {
+    //    $("#webticker").webTicker('cont');
+    //});
+
+    //$("#update").click(function () {
+    //    $("#webticker").webTicker('update', '<li id="item1">First News Item Updated</li><li id="item3">Third News Item Updated</li><li id="item4">Fourth News Item Updated</li><li id="item9">Ninth News Item Updated</li><li id="itemnew1">This is New Item 1</li><li  id="itemnew2">This is New Item 2</li><li  id="itemnew3">This is New Item 3</li><li  id="itemnew4">This is New Item 4</li>', 'swap');
+    //});
+
+    //$("#stop2").click(function () {
+    //    $("#webticker2").webTicker('stop');
+    //});
+
+    //$("#continue2").click(function () {
+    //    $("#webticker2").webTicker('cont');
+    //});
+});
